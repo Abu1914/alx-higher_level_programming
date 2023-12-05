680fcba0bbb095a2dc29bbbd4a19c0092a8afe76
Abu1914 <dmuktaru@gmail.com>
2023-12-04 19:20:21 -0800

    Task 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,4 +6,4 @@ def divisible_by_2(my_list=[]):
             new_list[len(new_list):] = [True]
         else:
             new_list[len(new_list):] = [False]
-return (new_list)
+    return (new_list)
