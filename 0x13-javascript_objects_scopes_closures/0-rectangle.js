f71c69cd28f3a8817334c29d7b9324327cefa35c
Abu1914 <dmuktaru@gmail.com>
2024-03-14 10:09:21 +0000

    Task on javascript-Object Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Defines an empty class rectangle
+
+class Rectangle {}
+// module.exports = class Rectangle {};
+// defines the empty class and exports it in a single line
+module.exports = Rectangle;
