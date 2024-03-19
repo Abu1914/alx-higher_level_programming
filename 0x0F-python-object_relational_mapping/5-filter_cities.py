49385ef95c2807c7f2c6f6499207cf55500beeb5
Abu1914 <dmuktaru@gmail.com>
2024-03-19 08:32:55 +0000

    5 filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-A script that lists all cities
-from the database hbtn_0e_4_usa
-Sorted in ascending by cities id
+    A script that lists all cities
+    from the database hbtn_0e_4_usa
+    Sorted in ascending by cities id
 """
 import MySQLdb
 from sys import argv
@@ -13,24 +13,24 @@ if __name__ == "__main__":
     executed if imported"""
 
     db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        database=argv[3])
-cur = db.cursor()
-query = """SELECT cities.name
-            FROM states
-            JOIN cities
-            ON states.id = cities.state_id
-            WHERE states.name LIKE %s
-            ORDER BY cities.id"""
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         database=argv[3])
+    cur = db.cursor()
+    query = """SELECT cities.name
+               FROM states
+               JOIN cities
+               ON states.id = cities.state_id
+               WHERE states.name LIKE %s
+               ORDER BY cities.id"""
 
-cur.execute(query, (argv[4], ))
-cities = []
-for city in cur.fetchall():
-    cities.append(city[0])
+    cur.execute(query, (argv[4], ))
+    cities = []
+    for city in cur.fetchall():
+        cities.append(city[0])
 
-print(", ".join(cities))
+    print(", ".join(cities))
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
