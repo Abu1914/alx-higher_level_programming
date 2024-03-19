7e619567dd62740c71a06e16f8101e3a0147329c
Abu1914 <dmuktaru@gmail.com>
2024-03-19 08:08:10 +0000

    SQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-A script that lists all states from the
-database hbtn_0e_0_usa
-The script takes 4 arguments
+    A script that lists all states from the
+    database hbtn_0e_0_usa
+    The script takes 4 arguments
 """
 import MySQLdb
 from sys import argv
@@ -13,10 +13,10 @@ if __name__ == "__main__":
     excuted if imported"""
 
     db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        database=argv[3])
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         database=argv[3])
     cur = db.cursor()
     query = """SELECT *
             FROM states
