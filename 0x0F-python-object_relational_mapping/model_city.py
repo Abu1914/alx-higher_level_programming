fda0a6d51cf0ac089b69669ad0b6934d8b923e98
Abu1914 <dmuktaru@gmail.com>
2024-03-19 10:10:28 +0000

    Cities in states

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Defines the class city
+    Defines the class city
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,10 +11,10 @@ Base = declarative_base()
 
 class City(Base):
     """
-    the class City
-    inherits from Base
+        the class City
+        inherits from Base
     """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-state_id = Column(Integer, ForeignKey(State.id), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
