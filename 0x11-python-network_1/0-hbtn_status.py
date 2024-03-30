53396f942efcc32b911e8116e5291a3a03c6cdc3
Abu1914 <dmuktaru@gmail.com>
2024-03-30 13:58:40 +0000

    whats my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-    this module fetches `https://alx-intranet.hbtn.io/status`
-"""
-from urlib.request import urlopen
 
 
-if__name__ == "__main__":
-    """prevents from being executed"""
-    with urlopen("https://alx-intranet.htbn.io/status") as response:
-        html = response.read()
+if __name__ == '__main__':
+    import urllib.request
 
-    print('Body response:')
-    print('\t- type:', type(html))
-    print('\t- content:', html)
-    print('\t- utf8 content:', html.decode('utf-8'))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
