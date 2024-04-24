2a0709ac1209d109634d4b9df8f218453a175c49
Abu1914 <dmuktaru@gmail.com>
2024-04-24 10:37:48 +0000

    How many completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const dic = {};
+    const tasks = JSON.parse(body);
+    tasks.forEach((task) => {
+      if (task.completed && dic[task.userId] === undefined) {
+        dic[task.userId] = 1;
+      } else if (task.completed) {
+        dic[task.userId]++;
+      }
+    });
+    console.log(dic);
+   }
+ });
